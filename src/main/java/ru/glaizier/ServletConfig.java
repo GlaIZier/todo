91032549b173534ffd0ebbb061a1be5c09ccdcf5
Mikhail Khokhlushin <mkhokhlushin@smile-net.ru>
2017-02-15 11:13:57 +0300

    Static resources handling and bootstrap have been added to the project

--- src/main/java/ru/glaizier/ServletConfig.java
+++ src/main/java/ru/glaizier/ServletConfig.java
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring4.SpringTemplateEngine;
@@ -43,10 +43,10 @@ public class ServletConfig extends WebMvcConfigurerAdapter {
         resolver.setTemplateMode(TemplateMode.HTML);
         return resolver;
     }
+
     @Override
-    public void configureDefaultServletHandling(
-            DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
 
 }
