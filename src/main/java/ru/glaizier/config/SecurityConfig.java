f8a760dfce9401547adc7d7f0bcabd7029af41f1
GlaIZier <kh.mv@bk.ru>
2017-02-27 22:28:48 +0300

    Basic inmemory authentication has been added to the project

--- src/main/java/ru/glaizier/config/SecurityConfig.java
+++ src/main/java/ru/glaizier/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package ru.glaizier.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,12 +13,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .anyRequest().authenticated()
+//        http
+//                .authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin().and()
+//                .httpBasic();
+        http.authorizeRequests()
+                .antMatchers("/tasks").hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/tasks").hasRole("USER")
+                .anyRequest().permitAll()
                 .and()
-                .formLogin().and()
-                .httpBasic();
+                .formLogin()
+                .loginPage("/login");
     }
 
+    // TODO add database authentication
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER").and()
+                .withUser("admin").password("password").roles("USER", "ADMIN");
+    }
 }
