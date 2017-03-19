facd694e0e351f1c61e21a84db1806483910b6d4
GlaIZier <kh.mv@bk.ru>
2017-03-19 14:38:48 +0300

    SecurityTest skeleton has been added

--- src/test/java/ru/glaizier/security/SecurityTest.java
+++ src/test/java/ru/glaizier/security/SecurityTest.java
@@ -1,7 +1,39 @@
 package ru.glaizier.security;
 
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import javax.servlet.Filter;
+
+// Todo understand how it works
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+@WebAppConfiguration
 public class SecurityTest extends Assert {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private Filter springSecurityFilterChain;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .addFilters(springSecurityFilterChain)
+                .build();
+    }
+
 
 }
