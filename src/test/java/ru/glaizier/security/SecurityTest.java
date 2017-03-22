36af63a88bdd788c9be67373f7adcae28012c97e
GlaIZier <kh.mv@bk.ru>
2017-03-22 16:50:56 +0300

    Logout test has been added to SecurityTest

--- src/test/java/ru/glaizier/security/SecurityTest.java
+++ src/test/java/ru/glaizier/security/SecurityTest.java
@@ -14,13 +14,14 @@ import ru.glaizier.config.SecurityConfig;
 import ru.glaizier.config.ServletConfig;
 
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.logout;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 // Todo start with understanding if i need servletConfig here and if why tests pass when securityconfig is empty
@@ -28,8 +29,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 // Todo check how RequestPostProcessors work
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {
-        SecurityConfig.class,
-        ServletConfig.class
+        ServletConfig.class,
+        SecurityConfig.class
 })
 @WebAppConfiguration
 public class SecurityTest {
@@ -47,36 +48,63 @@ public class SecurityTest {
     }
 
     @Test
-    public void shouldGetRootApiUnauthenticated() throws Exception {
+    public void getApiUnauthenticatedAndReturn401() throws Exception {
         mvc
                 .perform(get("/api/v1/tasks"))
+                .andDo(print())
                 .andExpect(status().isUnauthorized())
                 .andExpect(unauthenticated());
     }
 
     @Test
-    public void shouldGetRootApiAuthenticatedWhenUserIsPresent() throws Exception {
+    public void getApiAuthenticatedWhenUserIsPresent() throws Exception {
         mvc
                 .perform(get("/api/v1/tasks").with(user("u")))
+                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(authenticated().withUsername("u"));
     }
 
     @Test
-    public void shouldGetTasksUnauthenticatedAndGotoLogin() throws Exception {
+    public void getTasksUnauthenticatedAndRedirectToLogin() throws Exception {
         mvc
                 .perform(get("/tasks"))
+                .andDo(print())
                 .andExpect(status().isFound())
+                .andExpect(redirectedUrl("http://localhost/login"))
                 .andExpect(header().string("Location", equalTo("http://localhost/login")))
                 .andExpect(unauthenticated());
     }
 
     @Test
-    public void shouldGetTasksAuthenticatedAndGotoLogin() throws Exception {
+    public void getTasksAuthenticatedWhenUserIsPresent() throws Exception {
         mvc
                 .perform(get("/tasks").with(user("u")))
+                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(authenticated().withUsername("u"));
     }
+
+    @Test
+    public void getLogoutUnauthenticatedAndRedirectToRoot() throws Exception {
+        mvc
+                .perform(logout())
+                .andDo(print())
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(header().string("Location", equalTo("/")))
+                .andExpect(unauthenticated());
+    }
+
+    // Todo don't understand why it doesn't work. Looks like this context doesn't contain UserDetailsService. Try this work after enabling csrf
+//    @Test
+//    @WithMockUser
+//    public void testFormBasedAuthentication() throws Exception {
+//        mvc
+//                .perform(formLogin().user("admin").password("password"))
+//                .andDo(print())
+//                .andExpect(status().isFound())
+//                .andExpect(authenticated().withUsername("admin"));
+//    }
 
 }
