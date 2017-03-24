f16d9740493ce0809bf628bd87e16a67983c053b
GlaIZier <kh.mv@bk.ru>
2017-03-24 15:59:29 +0300

    Principal has been added to TasksRestController

--- src/main/java/ru/glaizier/controller/TasksRestController.java
+++ src/main/java/ru/glaizier/controller/TasksRestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.glaizier.domain.Task;
 
+import java.security.Principal;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,11 +13,12 @@ import java.util.List;
 @RequestMapping("/api/v1/")
 // todo create rest architecture
 // todo create react
+// Todo think about the best possible way for rest authentication
 public class TasksRestController {
 
     @RequestMapping(value = "/tasks", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Task> tasks() {
-        return Arrays.asList(new Task("Task1"), new Task("task2"));
+    public List<Task> tasks(Principal principal) {
+        return Arrays.asList(new Task(principal.getName() + "1"), new Task(principal.getName() + "2"));
     }
 
 }
