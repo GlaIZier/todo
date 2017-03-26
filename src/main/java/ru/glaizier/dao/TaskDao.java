ad4327e5a6624794bef55131cc7d8e0a53a1a5cb
GlaIZier <kh.mv@bk.ru>
2017-03-26 12:21:25 +0300

    TaskDao API has been updated

--- src/main/java/ru/glaizier/dao/TaskDao.java
+++ src/main/java/ru/glaizier/dao/TaskDao.java
@@ -4,42 +4,55 @@ import ru.glaizier.domain.Task;
 
 import java.util.List;
 
-// Todo refactor to handle tasks for specific user and think about design of Db
-// Todo update javadoc to handle situations with null
+// Todo add spring JPA interface
+// Todo move to repository pattern instead of Dao?
 public interface TaskDao {
 
     // collection
 
     /**
-     * @return List of Tasks
+     * @param login of user to get Tasks for
+     * @return List of Tasks or null if there is no tasks for this login or no such login
      */
-    List<Task> getTasks();
+    List<Task> getTasks(String login);
 
     /**
+     * @param login of user to create Task for
      * @param todo value for the task to create
-     * @return created Task
+     * @return created Task or null if there is no user with such login
      */
-    Task createTask(String todo);
+    Task createTask(String login, String todo);
 
     // specific task
 
     /**
+     * @param login of user to get Task for
      * @param id of the task
-     * @return Task for that id
+     * @return Task for that id or null if there is no task or no such login
      */
-    Task getTask(int id);
+    Task getTask(String login, int id);
 
     /**
+     * @param login of user to update Task for
      * @param id   of the task to update
      * @param todo value for the task to update
-     * @return updated Task
+     * @return updated Task or null if there is no task with this id or no such login
      */
-    Task updateTask(int id, String todo);
+    Task updateTask(String login, int id, String todo);
 
     /**
+     * @param login of user to remove Task for
      * @param id of task to delete
-     * @return deleted Task
+     * @return deleted Task or null if there is no task with this id or no such login
      */
-    Task removeTask(int id);
+    Task removeTask(String login, int id);
+
+    /**
+     * Use this method to understand what means null in other methods return (no login or another)
+     *
+     * @param login of user
+     * @return true if there is such login or false otherwise
+     */
+    boolean containsLogin(String login);
 
 }
