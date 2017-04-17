393b28c3f2d468c18c584a066528a2b5c58b769e
GlaIZier <kh.mv@bk.ru>
2017-04-17 19:21:42 +0300

    ApiSecurityConfig has been added for token validation

--- src/main/java/ru/glaizier/todo/config/root/SecurityConfig.java
+++ src/main/java/ru/glaizier/todo/config/root/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,94 +20,108 @@ import ru.glaizier.todo.security.filter.ApiTokenAuthenticationFilter;
 import javax.servlet.Filter;
 
 @EnableWebSecurity
-@Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private final AuthenticationSuccessHandler authenticationSuccessHandler;
-
-    @Value("${api.token.cookie.name}")
-    private String tokenCookieName;
-
-    // Todo move here all beans from security package not autowired
-    // Autowired bean declared in SecurityConfig above for remember-me function
-    @Autowired
-    public SecurityConfig(AuthenticationSuccessHandler authenticationSuccessHandler) {
-        this.authenticationSuccessHandler = authenticationSuccessHandler;
-    }
+public class SecurityConfig {
 
     @Bean
     public Filter apiFilter() {
         return new ApiTokenAuthenticationFilter();
     }
 
+    // Todo replace it with external classes
+    @Configuration
+    @Order(1)
+    public static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        private final AuthenticationSuccessHandler authenticationSuccessHandler;
 
-    @Bean
-    // TODO add database authentication and create inmemory dummy UserDetailsService for SecurityTest
-    // Todo add password encoding (hash + salt)
-    public UserDetailsService inMemoryUserDetailsService() throws Exception {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("u").password("p").roles("USER").build());
-        manager.createUser(User.withUsername("a").password("p").roles("USER", "ADMIN").build());
-        return manager;
+        @Value("${api.token.cookie.name}")
+        private String tokenCookieName;
+
+        // Todo move here all beans from security package not autowired
+        // Autowired bean declared in SecurityConfig above for remember-me function
+        @Autowired
+        public WebSecurityConfig(AuthenticationSuccessHandler authenticationSuccessHandler) {
+            this.authenticationSuccessHandler = authenticationSuccessHandler;
+        }
+
+
+        @Bean
+        // TODO add database authentication and create inmemory dummy UserDetailsService for SecurityTest
+        // Todo add password encoding (hash + salt)
+        public UserDetailsService inMemoryUserDetailsService() throws Exception {
+            InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+            manager.createUser(User.withUsername("u").password("p").roles("USER").build());
+            manager.createUser(User.withUsername("a").password("p").roles("USER", "ADMIN").build());
+            return manager;
+        }
+
+
+        @Override
+        // configure UserDetailsService
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(inMemoryUserDetailsService());
+            auth.eraseCredentials(false);
+        }
+
+        @Override
+        // Forbid goto login page when logined
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests() // Todo here add authentication Message provider
+                    // secure /tasks
+                    .antMatchers("/tasks").hasRole("USER")
+                    .antMatchers(HttpMethod.POST, "/tasks").hasRole("USER") // todo remove it?
+                    .anyRequest().permitAll()
+
+                    // login
+                    .and()
+                    .formLogin()
+                    .loginPage("/login")
+                    .failureUrl("/login?error")
+                    .usernameParameter("user")
+                    .passwordParameter("password")
+                    .successHandler(authenticationSuccessHandler)
+
+                    // logout
+                    .and()
+                    .logout()
+                    .logoutUrl("/logout")
+                    .logoutSuccessUrl("/")
+                    .invalidateHttpSession(true)
+                    // Session invalidation is called by default. remember-me-cookie is removed by default.
+                    // If it is added here then set-cookie header appears twice
+                    .deleteCookies(tokenCookieName)
+
+                    // remember me
+                    .and()
+                    .rememberMe()
+                    .key("todo-remember-me-key-random")
+                    .rememberMeParameter("remember-me")
+                    .rememberMeCookieName("todo-remember-me-cookie")
+                    .tokenValiditySeconds(180)
+                    .userDetailsService(inMemoryUserDetailsService()) // remember me requires explicitly defined UserDetailsService,
+
+                    // when ApiWebSecurityConfigurationAdapter and FormWebSecurityConfigurationAdapter don't (still don't know why)
+                    .and()
+                    .requiresChannel()
+                    .antMatchers("/")
+                    .requiresSecure()
+
+                    // csrf
+                    .and()
+                    .csrf() // todo enable csrf, add csrf to login and register page markup and create logout using POST http method
+                    .disable();
+        }
     }
 
+    @Configuration
+    @Order(2)
+    public class ApiSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    // configure UserDetailsService
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(inMemoryUserDetailsService());
-        auth.eraseCredentials(false);
-    }
-
-    @Override
-    // Forbid goto login page when logined
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests() // Todo here add authentication Message provider
-                // secure /tasks
-                .antMatchers("/tasks").hasRole("USER")
-                .antMatchers(HttpMethod.POST, "/tasks").hasRole("USER") // todo remove it?
-                .anyRequest().permitAll()
-
-                // login
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .failureUrl("/login?error")
-                .usernameParameter("user")
-                .passwordParameter("password")
-                .successHandler(authenticationSuccessHandler)
-
-                // logout
-                .and()
-                .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/")
-                .invalidateHttpSession(true)
-                // Session invalidation is called by default. remember-me-cookie is removed by default.
-                // If it is added here then set-cookie header appears twice
-                .deleteCookies(tokenCookieName)
-
-                // remember me
-                .and()
-                .rememberMe()
-                .key("todo-remember-me-key-random")
-                .rememberMeParameter("remember-me")
-                .rememberMeCookieName("todo-remember-me-cookie")
-                .tokenValiditySeconds(180)
-                .userDetailsService(inMemoryUserDetailsService()) // remember me requires explicitly defined UserDetailsService,
-
-                // when ApiWebSecurityConfigurationAdapter and FormWebSecurityConfigurationAdapter don't (still don't know why)
-                .and()
-                .requiresChannel()
-                .antMatchers("/")
-                .requiresSecure()
-
-                // csrf
-                .and()
-                .csrf() // todo enable csrf, add csrf to login and register page markup and create logout using POST http method
-                .disable();
-
-        http.addFilterAfter(apiFilter(), BasicAuthenticationFilter.class); // not sure that it is needed to be inserted after BasicAuthenticationFilter
-
+        //Todo add custom filter after logout to invalidate token
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.antMatcher("/api/**")
+                    .addFilterAfter(apiFilter(), BasicAuthenticationFilter.class); // Todo check after what filter add my
+            // not sure that it is needed to be inserted after BasicAuthenticationFilter
+        }
     }
 }
--- src/main/java/ru/glaizier/todo/security/filter/ApiTokenAuthenticationFilter.java
+++ src/main/java/ru/glaizier/todo/security/filter/ApiTokenAuthenticationFilter.java
@@ -2,18 +2,19 @@ package ru.glaizier.todo.security.filter;
 
 import org.springframework.web.filter.GenericFilterBean;
 
+import java.io.IOException;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
-import java.io.IOException;
 
 @WebFilter(urlPatterns = "/api/*")
 public class ApiTokenAuthenticationFilter extends GenericFilterBean {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.out.println("Hello from custom filter");
+        System.out.println("Hello from api token filter");
         chain.doFilter(request, response);
     }
 }
