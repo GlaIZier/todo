547d2ea78d88d10594c2c3e7555852561125746c
GlaIZier <kh.mv@bk.ru>
2017-04-27 19:27:25 +0300

    Several tests have been added to ApiSecurityIntegrationTest

--- src/test/java/ru/glaizier/todo/test/security/ApiSecurityIntegrationTest.java
+++ src/test/java/ru/glaizier/todo/test/security/ApiSecurityIntegrationTest.java
@@ -6,6 +6,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +16,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 import ru.glaizier.todo.config.root.RootConfig;
 import ru.glaizier.todo.config.servlet.ServletConfig;
+import ru.glaizier.todo.properties.PropertiesService;
+import ru.glaizier.todo.security.token.TokenService;
 
 import javax.servlet.http.Cookie;
 
@@ -31,8 +33,16 @@ import javax.servlet.http.Cookie;
 @WebAppConfiguration
 public class ApiSecurityIntegrationTest {
 
+    private static final String testUri = "/api/me/tasks";
+
     @Autowired
     private WebApplicationContext context;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private PropertiesService propertiesService;
 
     private MockMvc mvc;
 
@@ -44,16 +54,51 @@ public class ApiSecurityIntegrationTest {
                 .build();
     }
 
-    private final RestTemplate restTemplate = new RestTemplate();
-
     @Test
-    // Todo start here
-    public void return401WhenTokenCookieIsNotPresent() throws Exception {
+    public void return401WhenTokenCookieIsMissing() throws Exception {
         // need to provide dummy cookie or we will get NullPointerException while getting cookies in filter:
         // req.getCookies
-        mvc.perform(get("/api/me/tasks").cookie(new Cookie("dummy", "dummy")))
+        mvc.perform(get(testUri).cookie(new Cookie("dummy", "dummy")))
                 .andDo(print())
                 .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void get401WhenTokenSignatureIsInvalid() throws Exception {
+        String token = tokenService.createToken("dummyLogin");
+        String invalidToken = token.substring(0, token.length() - 3);
+        invalidToken = invalidToken + token.charAt(token.length() - 2) + token.charAt(token.length() - 1) +
+                token.charAt(token.length() - 1);
+
+        mvc.perform(get(testUri).cookie(new Cookie(propertiesService.getApiTokenCookieName(), invalidToken)))
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+
+    //    @Test
+//    public void get400WhenTokenIsCorrupted() {
+//        String token = agentJWTEncoder.encodeSystemAgent("testId", "testMnemonicId");
+//        // corrupted token because we replace last = with prev symbol
+//        String corruptedToken = token.substring(0, token.length() - 2);
+//        corruptedToken = corruptedToken + token.charAt(token.length() - 1) + token.charAt(token.length() - 2);
+//
+//        ClientHttpRequestInterceptor interceptor = new ApiClientHeaderAdder(apiAuthTokenHeaderName, corruptedToken);
+//        restTemplate.getRestTemplate().setInterceptors(Collections.singletonList(interceptor));
+//
+//        ResponseEntity<String> deliveryResponse = restTemplate.getForEntity(
+//                DELIVERY_LICENSES_OBOOK_ONIX_LICENSE_ID_URL, String.class);
+//        assertThat(deliveryResponse.getStatusCode(), is(HttpStatus.BAD_REQUEST));
+//    }
+//
+    @Test
+    @Ignore
+    // Add mockito amd avoid NPE in taskDao
+    public void get200WhenTokenIsOk() throws Exception {
+        String token = tokenService.createToken("dummyLogin");
+
+        mvc.perform(get(testUri).cookie(new Cookie(propertiesService.getApiTokenCookieName(), token)))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
 }
