b1f4b0bc92a2cf8601c4270530764f9239e49dd1
GlaIZier <kh.mv@bk.ru>
2017-10-08 13:54:22 +0300

    Tasks list view has been added to TaskController

--- src/main/java/ru/glaizier/todo/controller/view/TaskController.java
+++ src/main/java/ru/glaizier/todo/controller/view/TaskController.java
@@ -1,16 +1,34 @@
 package ru.glaizier.todo.controller.view;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.glaizier.todo.model.dto.TaskDto;
+import ru.glaizier.todo.persistence.Persistence;
+
+import java.util.List;
 
 @Controller
 public class TaskController {
 
+    private Persistence persistence;
+
+    @Autowired
+    public TaskController(Persistence persistence) {
+        this.persistence = persistence;
+    }
+
     @RequestMapping("/tasks")
     @Secured({"ROLE_USER", "ROLE_ADMIN"})
-    public String tasks() {
+    // Todo check why couldn't find session on mac when not incognito window
+    public String tasks(@AuthenticationPrincipal User activeUser, Model model) {
+        List<TaskDto> tasks = persistence.findTasks(activeUser.getUsername());
+        model.addAttribute("tasks", tasks);
         return "tasks";
     }
 
