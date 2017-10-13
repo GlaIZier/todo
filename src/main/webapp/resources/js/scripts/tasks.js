9c5a44c88005fd8967710ddf4b03030ea8d78648
GlaIZier <kh.mv@bk.ru>
2017-10-13 12:04:30 +0300

    Tasks.js has been added

--- src/main/webapp/resources/js/scripts/tasks.js
+++ src/main/webapp/resources/js/scripts/tasks.js
@@ -1,1 +1,27 @@
-console.log("Hello!");
+// Todo add error notifications label
+var Task = Task || (function () {
+
+    return {
+      saveTask: function () {
+        var newTodo = $('#new-task-input').val();
+        var apiToken = $.cookie(config.apiTokenCookieName);
+
+        var headers = {};
+        headers[config.apiTokenHeaderName] = apiToken;
+
+        $.ajax({
+          type: 'POST',
+          url: config.apiBaseUrl + config.meTasksEndpoint,
+          headers: headers,
+          data: newTodo
+        })
+          .done(function (response, status, jq) {
+            console.log(response);
+          })
+          .fail(function (xhr, status, error) {
+            console.error(error);
+          });
+      }
+    }
+
+  }());
