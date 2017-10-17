095686c0e7ae7b0281b92397e46f7a08162a8729
GlaIZier <kh.mv@bk.ru>
2017-10-17 10:11:23 +0300

    Creating task in js has been finished

--- src/main/webapp/resources/js/scripts/tasks.js
+++ src/main/webapp/resources/js/scripts/tasks.js
@@ -1,6 +1,7 @@
 // Todo add error notifications label
 var Task = Task || (function () {
 
+
     return {
       saveTask: function () {
         var newTodo = $('#new-task-input').val();
@@ -18,12 +19,18 @@ var Task = Task || (function () {
           data: 'todo=' + newTodo
         })
           .done(function (response, status, jq) {
-            var newTask = {};
-            newTask = response.data;
-            console.log(newTask);
+            var newTask = response.data;
+            var newTaskElement =
+              '<div id="' + newTask.id + '" class="todo well well-sm">' +
+              '<span class="todo-text">' + newTask.todo + '</span>' +
+              '<span class="todo-remove clickable glyphicon glyphicon-remove" aria-hidden="true"></span>' +
+              '</div>';
+            $('#todos').append(newTaskElement);
+            console.log("Created task: " + JSON.stringify(newTask));
           })
           .fail(function (xhr, status, error) {
             console.error(error);
+            alert("Error: " + error + ". Try to reload the page");
           });
       }
     }
