b6f593e32c7ebdcebd254b16442fda8ad6e9947e
GlaIZier <khokh.mv@gmail.com>
2018-01-24 20:11:52 +0300

    [#3] SpaHomeControllerTest has been added

---
+++ src/test/java/ru/glaizier/todo/test/controller/view/spa/SpaHomeControllerTest.java
@@ -0,0 +1,54 @@
+package ru.glaizier.todo.test.controller.view.spa;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import ru.glaizier.todo.config.root.RootConfig;
+import ru.glaizier.todo.config.servlet.ServletConfig;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {
+        ServletConfig.class,
+        RootConfig.class
+})
+@WebAppConfiguration
+public class SpaHomeControllerTest {
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    public void redirectToHttps() throws Exception {
+        mvc.perform(get("/spa"))
+            .andExpect(status().isFound());
+    }
+
+    @Test
+    public void showForHttps() throws Exception {
+        mvc.perform(get("/spa").secure(true))
+                .andExpect(status().isOk())
+                .andExpect(view().name("spa"));
+    }
+
+}
